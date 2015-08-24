aa5748865e0c13242c2227880bbe09f23a9a777c
Michael Paulson <mpaulson@netflix.com>
2015-08-24 07:57:13 -0700

    added an extra method so that the code can live in the path utils and not in falcor.

--- test/pathsComplementFromTree.spec.js
+++ test/pathsComplementFromTree.spec.js
@@ -1,7 +1,8 @@
-var pathsComplementFromTree = require('./../lib/pathsComplementFromTree');
+var pathsComplementFromTree = require('./../lib').pathsComplementFromTree;
+var pathsComplementFromLengthTree = require('./../lib').pathsComplementFromLengthTree;
 var expect = require('chai').expect;
 
-describe('pathsComplementFromTree', function() {
+describe('pathsComplementFromTree and LengthTree', function() {
     it('should strip the single path from tree.', function() {
         var paths = [['one', 'two']];
         var tree = {one: {two: null}};
@@ -32,6 +33,41 @@ describe('pathsComplementFromTree', function() {
             }
         };
         var out = pathsComplementFromTree(paths, tree);
+        expect(out).to.deep.equals([['one', {from: 0, to: 2}, 'two']]);
+    });
+
+    it('should strip the single path from length tree.', function() {
+        var paths = [['one', 'two']];
+        var tree = {2: {one: {two: null}}};
+        var out = pathsComplementFromLengthTree(paths, tree);
+        expect(out).to.deep.equals([]);
+    });
+
+    it('should not strip the single path from length tree.', function() {
+        var paths = [['one', 'two']];
+        var tree = {2: {one: {too: null}}};
+        var out = pathsComplementFromLengthTree(paths, tree);
+        expect(out).to.deep.equals([['one', 'two']]);
+    });
+
+    it('should strip out one of the two paths, has complex paths from length tree.', function() {
+        var paths = [
+            ['one', {from: 0, to: 1}, 'two'],
+            ['one', {from: 0, to: 2}, 'two']
+        ];
+        var tree = {
+            3: {
+                one: {
+                    0: {
+                        two: null
+                    },
+                    1: {
+                        two: null
+                    }
+                }
+            }
+        };
+        var out = pathsComplementFromLengthTree(paths, tree);
         expect(out).to.deep.equals([['one', {from: 0, to: 2}, 'two']]);
     });
 });
