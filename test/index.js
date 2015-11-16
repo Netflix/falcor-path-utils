e0dc0a8dedc1e027790020109daf1bdf5fa1e020
Michael Paulson <mpaulson@netflix.com>
2015-11-16 07:05:01 -0800

    Updated falcor-path-utils to have some of the routers logic around optimizing path sets.

--- test/index.js
+++ test/index.js
@@ -2,4 +2,5 @@ describe('falcor-path-utils', function() {
     require('./toPaths.spec');
     require('./toTree.spec');
     require('./pathsComplementFromTree.spec');
+    require('./optimizePathSets.spec');
 });
