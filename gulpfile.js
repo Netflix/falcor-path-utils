1f7cecd91495cf4ae48eb49794550e5c939c0782
Peter Marton <pmarton@netflix.com>
2019-01-02 14:38:09 -0800

    chore(gulp): fix runner for Node 10.x

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,7 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 
-gulp.task('test', function (cb) {
-  gulp.src(['./test/index.js'])
-    .pipe(mocha())
-    .on('end', cb);
+gulp.task('test', function () {
+  return gulp.src(['./test/index.js'])
+    .pipe(mocha());
 });
